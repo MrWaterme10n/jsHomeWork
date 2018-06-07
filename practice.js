6a997ddeb5390947a6a1f856fd4ded88a83e6d60
Igor <newfinancelife@gmail.com>
2018-06-07 21:08:54 +0300

    Это наш первый комит

---
+++ practice.js
@@ -0,0 +1,14 @@
+var budget = prompt('Ваш бюджет?');
+var shopName = prompt('Название вашего магазина?');
+var shopGoods = [];
+var mainList = {
+    budget,
+    shopName,
+    shopGoods ,
+    // employers,
+    // open
+}
+
+var shopGoods = []
+console.log(mainList['budget']);
+console.log(mainList.shopName);
